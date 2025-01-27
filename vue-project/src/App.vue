11add37ad73e81dfe4b3a32ff45e7790027c96dc
AndreiTorres-0417 <thesecondhero0417@gmail.com>
2025-01-27 23:13:13 +0800

    g

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,10 +2,10 @@
 
 <template>
   <h1>Food</h1>
-  <food-item /> <br>
-  <food-item2 /> <br>
-  <food-item /> <br>
-  <food-item2 /> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
 </template>
 
 <style></style>
