a185d37792f630c219c3bd386762a64a117f39c1
AndreiTorres-0417 <thesecondhero0417@gmail.com>
2025-01-31 09:15:13 +0800

    we

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://<project>.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdranBrc3Bmd21vcnJicWtzdmJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjU5ODUsImV4cCI6MjA1MzYwMTk4NX0.TlnQll4NQuprl4VmTzIFBTrQs8xPgAUFA9Pgw9Qyouk>')
+export const supabase = createClient('https://<project>.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpeHJqemhodmJ1eXhjandodWhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjY4ODgsImV4cCI6MjA1MzYwMjg4OH0.ScfVoAFUh-LSkB4g64zJaqPXFeZcOE5lZ0pwUNmPQQo>')
