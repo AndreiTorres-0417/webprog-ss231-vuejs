64b281f6db6aca4a4f1ebb84e3f6562f72ac474b
AndreiTorres-0417 <thesecondhero0417@gmail.com>
2025-02-06 13:00:44 +0000

    comment

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,11 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-// Fetch instruments from Supabase
-const instruments = ref([])
-
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-
-onMounted(() => {
-  getInstruments()
-})
-</script>
-
 <template>
-  <div>
-    <!-- Instruments List -->
-    <h1>Instruments</h1>
-    <ul>
-      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-    </ul>
-
-    <!-- Food Components -->
-    <h1>Food</h1>
-    <food-item />
-    <food-item2 />
-    <food-item />
-    <food-item2 />
-
-    <!-- Personal Profile Component -->
-    <h1>Personal Profile</h1>
-    <personal-profile />
-  </div>
+  <h1>Food</h1>
+  <country />
+  <food-item />
+  <food-item />
+  <food-item />
+  <comment-form />
+  <comment />
 </template>
 
-<script>
-export default {
-  name: 'App'
-};
-</script>
-
-<style>
-/* Add any global styles here if needed */
-</style>
+<script></script>
